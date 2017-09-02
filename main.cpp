6ff6b7a98d8b0b578d262b6dec9d53be5f1495c3
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-09-02 15:26:48 -0300

    adicionei os testes do quicksort

--- main.cpp
+++ main.cpp
@@ -3,25 +3,48 @@
 #include <string>
 #include "stdlib.h"
 #include "tweets.h"
+#include "QuickSort.h"
 
 using namespace std;
 
 int main()
 {
-    tweets* vetor[2];
-    int id1 = 11;
-    int tid1 = 121;
-    string t = "teste de tweet1";
-    string d = "2017-08-30 16:31:42";
+	// Instanciando 7 tweets para teste
+	tweets* vetor[7];
+	int id1 = 11;
+	int tid1 = 121;
+	string t = "teste de tweet1";
+	string d = "2017-08-30 16:31:42";
+	vetor[0] = new tweets(id1, tid1, t, d);
+	vetor[1] = new tweets(50, 14, t, d);
+	vetor[2] = new tweets(98, 41, t, d);
+	vetor[3] = new tweets(47, 98, t, d);
+	vetor[4] = new tweets(26, 16, t, d);
+	vetor[5] = new tweets(64, 189, t, d);
+	vetor[6] = new tweets(54, 28, t, d);
 
-    for (int i=0;i<3;i++)
-    {
-        vetor[i] = new tweets(id1,tid1,t,d);
-    }
+	/* Começo das funçoes apenas para testes do quicksort */
+	cout << "antes de ordenar" << endl;
+	for (int k = 0; k < 7; k++)
+	{
+		cout << vetor[k]->getTweetID() << endl;
+	}
+		
+	QuickSort qs; //Chama a classe
+	qs.quicksort(vetor, 0, 6); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final
+	int trocas = qs.getNumTrocas();
+	int comp = qs.getNumComparacoes();
 
-    cout<<"ID do usuario: "<<vetor[0]->getuserID()<<endl;
-    cout<<"ID do tweet: "<<vetor[0]->gettweetID()<<endl;
-    cout<<"Tweet: "<<vetor[0]->getTweet()<<endl;
-    cout<<"Data: "<<vetor[0]->getDate()<<endl;
-    return 0;
+	cout << "depois de ordenar" << endl;
+	for (int p = 0; p < 7; p++)
+	{
+		cout << vetor[p]->getTweetID() << endl;
+	}
+
+	cout << "numero de trocas: " << trocas << endl;
+	cout << "numero de comparacoes: " << comp << endl;
+	/* Fim das funçoes apenas para testes do quicksort */
+
+	//system("pause"); //coloquei isso pq se nao fica sumindo a janela no visual studio, pode tirar
+	return 0;
 }
