5b37c0cb7e7f66834bb3daf45eb3be262e6257d1
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-09-02 16:10:49 -0300

    classe do quicksort
    adicionei o relogio e numero de comparacoes

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 /*
 ainda falta:
-colocar o clock para calcular o tempo gasto na ordenaçao 
-calcular o numero de comparaçoes feitas
 escrever mais comentarios
-implementar a funcao para ler do arquivo entrada.txt para fazer a ordenaçao aleatoria dos tweets
+implementar a funcao para ler do arquivo entrada.txt para fazer a ordenaÃ§ao aleatoria dos tweets
+implementar a funcao para escrever os resultados no arquivo saida.txt
 alterar o codigo para funcionar no numero N de vezes lidos da entrada.txt
 */
 
@@ -15,6 +14,7 @@ QuickSort::QuickSort()
 {
 	numTrocas = 0;
 	numCompar = 0;
+	tempoGasto = 0.0;
 }
 
 //Destrutor
@@ -34,15 +34,23 @@ int QuickSort::getNumComparacoes()
 	return numCompar;
 }
 
+double QuickSort::getTempoGasto()
+{
+	return tempoGasto;
+}
+
 //Nao usei o std::swap para poder adicionar o de numero de trocas
 void QuickSort::troca(tweets* &t1, tweets* &t2)
 {
-	//uso isso so pra testar, nao precisa. vou tirar quando tiver tudo pronto 100%
-	//cout << "trocando " << t1->getTweetID() << " por " << t2->getTweetID() << endl;
-	tweets* aux = t1;
-	t1 = t2;
-	t2 = aux;
-	numTrocas++;
+	if (t1 != t2) //Nao troca se t1 e t2 sao iguais, ja que nao precisa
+	{
+		//uso isso so pra testar, nao precisa. vou tirar quando tiver tudo pronto 100%
+		//cout << "trocando " << t1->getTweetID() << " por " << t2->getTweetID() << endl;
+		tweets* aux = t1;
+		t1 = t2;
+		t2 = aux;
+		numTrocas++;
+	}
 }
 
 //Algoritmo de particionamento do vetor
@@ -61,11 +69,13 @@ int QuickSort::particiona(tweets* vet[], int inicio, int fim)
 		if (vet[esq]->getTweetID() <= pivo)
 		{
 			esq++;//Caminha da esquerda para a direita
+			numCompar++;
 		}
 		else
 		{
 			while ((esq != dir) && (pivo < vet[dir]->getTweetID()))
 				{
+					numCompar++;
 					dir--; //Caminha da direita para a esquerda
 				}
 			troca(vet[esq], vet[dir]); //Faz a troca
@@ -74,20 +84,24 @@ int QuickSort::particiona(tweets* vet[], int inicio, int fim)
 
 	if (vet[esq]->getTweetID() > pivo)
 	{
-		esq--; //Continua caminhando da esquerda para a direita
+		esq--; //Volta
+		numCompar++;
 	}
 
 	troca(vet[inicio], vet[esq]); //Se mudar o pivo, nao esquecer de mudar o pivo (vet[inicio]) nessa troca tambem
-	return (esq); //Retorna a posiçao ordenada
+	return (esq); //Retorna a posiÃ§ao ordenada
 }
 
 //Algoritmo do quicksort recursivo
 void QuickSort::quicksort(tweets* vet[], int ini, int fim)
 {
+	clock_t relogio;
+	relogio = clock();
 	if (ini < fim) 
 	{
 		int part = particiona(vet, ini, fim);
 		quicksort(vet, ini, part-1);
 		quicksort(vet, part+1, fim);
 	}
+	tempoGasto = (clock() - relogio) / (double)CLOCKS_PER_SEC;
 }
