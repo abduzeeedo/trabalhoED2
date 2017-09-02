88f6ccee14b2b10a228d01641bab6487aed28bab
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-09-02 16:11:17 -0300

    classe do quicksort
    adicionei o relogio e o numero de comparacoes

--- QuickSort.h
+++ QuickSort.h
@@ -1,6 +1,7 @@
 #include "tweets.h"
 #include <iostream>
-#include <stdlib.h>
+#include <ctime>
+
 class QuickSort
 {
 private:
@@ -8,11 +9,12 @@ private:
 	void troca(tweets* &t1, tweets* &t2);
 	int numTrocas;
 	int numCompar;
+	double tempoGasto;
 public:
 	QuickSort();
 	~QuickSort();
 	void quicksort(tweets* vetor[], int ini, int fim);
 	int getNumTrocas();
 	int getNumComparacoes();
+	double getTempoGasto();
 };
-
