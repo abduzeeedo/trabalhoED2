4733cf10651acd2affe0bb043f9a0f8e29fa4d20
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-09-03 15:10:44 -0300

    main com chamada pro mergesort

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "tweets.h"
 #include "QuickSort.h"
 #include "InsertionSort.h"
+#include "MergeSort.h"
 
 using namespace std;
 
@@ -47,7 +48,7 @@ void imprimeMenu()
 void codigoFuncao() {
 	string code;
 	while (true) {
-		cout << "Insira o Codigo de Função: (-1 para Encerrar Execução)" << endl;
+		cout << "Insira o Codigo de FunÃ§Ã£o: (-1 para Encerrar ExecuÃ§Ã£o)" << endl;
 		cin >> code;
 		if (code == "-1") {
 			return;
@@ -78,30 +79,34 @@ int main()
 	vetor[5] = new tweets(64, 189, t, d);
 	vetor[6] = new tweets(54, 28, t, d);
 
-	// Começo das funçoes apenas para testes de ordenacao
+	// ComeÃ§o das funÃ§oes apenas para testes de ordenacao
 	cout << "Antes de ordenar:" << endl;
 
-	for (int k = 0; k < 7; k++)
+	for (int k = 0; k < tam; k++)
 	{
 		cout << "[" << vetor[k]->getTweetID() << "]";
 	}
 
-	/* Deixar somente uma ordenaçao sem estar comentada, se não todas as outras irão pegar o vetor já ordenado pela primeira para ordenar */
+	/* Deixar somente uma ordenaÃ§ao sem estar comentada, se nÃ£o todas as outras irÃ£o pegar o vetor jÃ¡ ordenado pela primeira para ordenar */
 
 	/*QuickSort*/
 	//QuickSort ordena; //Chama a classes
-	//ordena.quicksort(vetor, 0, 6, 'r'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final
+	//ordena.quicksort(vetor, 0, tam-1, 'r'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final
 
 	/*InsertionSort*/
-	InsertionSort ordena; //Chama a classe
-	ordena.insertionsort(vetor, 0, 7); //Faz o insertionsort. Passar 0 para ordenar desde o inicio e o tamanho total, nao tamanho-1
+	//InsertionSort ordena; //Chama a classe
+	//ordena.insertionsort(vetor, 0, tam); //Faz o insertionsort. Passar 0 para ordenar desde o inicio e o tamanho total, nao tamanho-1
 
+	/*MergeSort*/
+	MergeSort ordena; //Chama a classe 
+	ordena.mergesort(vetor, 0, tam-1); //Faz o mergesort. Passar sempre 0 como inicio e tamanho-1 como final
+	
 	/*BubbleSort*/
 	//BubbleSort ordena; //Chama a classe
 	//ordena.bubblesort(vetor, tam); //Faz o bubblesort. Passar o tamanho total, nao tamanho-1
 
 	cout << endl << "Depois de ordenar:" << endl;
-	for (int p = 0; p < 7; p++)
+	for (int p = 0; p < tam; p++)
 	{
 		cout << "[" << vetor[p]->getTweetID() << "]";
 	}
@@ -109,10 +114,10 @@ int main()
 	cout << endl << endl;
 	cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
 	cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
-	cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos, tenho que testar com mais.
-	// Fim das funçoes apenas para testes de ordenacao
+	cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaÃ§ao estÃ¡ muito rapida com poucos elementos, tenho que testar com mais.
+	// Fim das funÃ§oes apenas para testes de ordenacao
 
-	codigoFuncao(); //Seleciona a funcao ou encerra a execução;
+	codigoFuncao(); //Seleciona a funcao ou encerra a execuÃ§Ã£o;
 	//system("pause"); //coloquei isso pq se nao fica sumindo a janela no visual studio, pode tirar
 	return 0;
 }
