1f5707caa536727231614c8d07a09c4781695fcd
Bruno Carvalho <brunobcarvalho@live.com>
2017-09-03 16:06:06 -0300

    codigoFuncao adiantada, mas n completa

--- main.cpp
+++ main.cpp
@@ -44,11 +44,20 @@ void imprimeMenu()
 	leitor.close();
 }
 
+void imprimeVetor(tweets* vet[], int tam) {
+	cout << "Vetor: ";
+	for (int k = 0; k < tam; k++)
+	{
+		cout << "[" << vet[k]->getTweetID() << "]";
+	}
+	cout << endl;
+}
+
 //Metodo que seleciona a funcao a ser executada e finaliza execucao
-void codigoFuncao() {
+void codigoFuncao(tweets* vet[], int tam) {
 	string code;
 	while (true) {
-		cout << "Insira o Codigo de FunÃ§Ã£o: (-1 para Encerrar ExecuÃ§Ã£o)" << endl;
+		cout << endl << "Insira o Codigo de Funcao: (-1 para Encerrar Execução)" << endl;
 		cin >> code;
 		if (code == "-1") {
 			return;
@@ -56,13 +65,68 @@ void codigoFuncao() {
 		if (code == "0") {
 			imprimeMenu();
 		}
+		if (code == "1") {
+			/*QuickSort Recursivo Padrao*/
+			cout << "Antes de Ordenar via QuickSort:" << endl;
+			imprimeVetor(vet, tam); 
+			cout << endl;
+
+			QuickSort ordena; //Chama a classes
+			ordena.quicksort(vet, 0, tam-1, 'r'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final. Tipo 'r'-> Recursivo Padrao
+
+			cout << "Depois de Ordenar:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			//Resultado das Operações
+			cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
+			cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
+			cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos, tenho que testar com mais.
+		}
+
+		if (code == "2") {
+			/*QuickSort com Mediana de 3 valores*/
+			cout << "Antes de Ordenar via QuickSort:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			QuickSort ordena; //Chama a classes
+			ordena.quicksort(vet, 0, tam - 1, 'm'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final. Tipo 'm'-> mediana de 3 valores
+
+			cout << "Depois de Ordenar:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			//Resultado das Operações
+			cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
+			cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
+			cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos, tenho que testar com mais.
+		}
+		if (code == "3") {
+			/*QuickSort com Mediana de 5 valores*/
+			cout << "Antes de Ordenar via QuickSort:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			QuickSort ordena; //Chama a classes
+			ordena.quicksort(vet, 0, tam - 1, 'M'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final. Tipo 'M'-> mediana de 3 valores
+
+			cout << "Depois de Ordenar:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			//Resultado das Operações
+			cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
+			cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
+			cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos, tenho que testar com mais.
+		}
 	}
 }
 
 int main()
 {
 
-	//imprimeMenu(); // Funcao para imprimir o Menu no Console
+	imprimeMenu(); // Funcao para imprimir o Menu no Console
 
 	// Instanciando 7 tweets para teste
 	tweets* vetor[7];
@@ -79,45 +143,20 @@ int main()
 	vetor[5] = new tweets(64, 189, t, d);
 	vetor[6] = new tweets(54, 28, t, d);
 
-	// ComeÃ§o das funÃ§oes apenas para testes de ordenacao
-	cout << "Antes de ordenar:" << endl;
-
-	for (int k = 0; k < tam; k++)
-	{
-		cout << "[" << vetor[k]->getTweetID() << "]";
-	}
-
-	/* Deixar somente uma ordenaÃ§ao sem estar comentada, se nÃ£o todas as outras irÃ£o pegar o vetor jÃ¡ ordenado pela primeira para ordenar */
-
-	/*QuickSort*/
-	//QuickSort ordena; //Chama a classes
-	//ordena.quicksort(vetor, 0, tam-1, 'r'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final
-
 	/*InsertionSort*/
 	//InsertionSort ordena; //Chama a classe
 	//ordena.insertionsort(vetor, 0, tam); //Faz o insertionsort. Passar 0 para ordenar desde o inicio e o tamanho total, nao tamanho-1
 
 	/*MergeSort*/
-	MergeSort ordena; //Chama a classe 
-	ordena.mergesort(vetor, 0, tam-1); //Faz o mergesort. Passar sempre 0 como inicio e tamanho-1 como final
-	
+	//MergeSort ordena; //Chama a classe 
+	//ordena.mergesort(vetor, 0, tam - 1); //Faz o mergesort. Passar sempre 0 como inicio e tamanho-1 como final
+
 	/*BubbleSort*/
 	//BubbleSort ordena; //Chama a classe
 	//ordena.bubblesort(vetor, tam); //Faz o bubblesort. Passar o tamanho total, nao tamanho-1
 
-	cout << endl << "Depois de ordenar:" << endl;
-	for (int p = 0; p < tam; p++)
-	{
-		cout << "[" << vetor[p]->getTweetID() << "]";
-	}
 
-	cout << endl << endl;
-	cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
-	cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
-	cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaÃ§ao estÃ¡ muito rapida com poucos elementos, tenho que testar com mais.
-	// Fim das funÃ§oes apenas para testes de ordenacao
-
-	codigoFuncao(); //Seleciona a funcao ou encerra a execuÃ§Ã£o;
+	codigoFuncao(vetor, tam); //Seleciona a funcao ou encerra a execução;
 	//system("pause"); //coloquei isso pq se nao fica sumindo a janela no visual studio, pode tirar
 	return 0;
 }
