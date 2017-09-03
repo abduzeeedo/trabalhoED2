bd987be1ff6e4557e2a57da4e8fbe362c97e0466
Bruno Carvalho <brunobcarvalho@live.com>
2017-09-03 11:41:57 -0300

    Chamada de Funcao QuickSort com "tipo"

--- main.cpp
+++ main.cpp
@@ -2,24 +2,24 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include "stdlib.h"
 #include "tweets.h"
 #include "QuickSort.h"
-#include "BubbleSort.h"
 #include "InsertionSort.h"
 
 using namespace std;
 
 //Funcao usada para pegar as linhas do arquivo do Menu
-string parseart(ifstream& File)
+std::string parseart(std::ifstream& File)
 {
-	string parsedfile;
+	std::string parsedfile;
 
 	if (File)
 	{
 		while (File.good())
 		{
-			string tmpline;
-			getline(File, tmpline);
+			std::string tmpline;
+			std::getline(File, tmpline);
 			tmpline += "\n";
 
 			parsedfile += tmpline;
@@ -29,15 +29,16 @@ string parseart(ifstream& File)
 	else
 	{
 		cout << "ERRO AO LER O ARQUIVO DE MENU" << endl;
+		return 0;
 	}
 }
 
 //Funcao de Impressao de Menu
 void imprimeMenu()
 {
-	ifstream leitor("menu.txt"); //Le o menu to txt
-	string art = parseart(leitor);
-	cout << art << endl; //Imprime o menu
+	std::ifstream leitor("menu.txt"); //Le o menu to txt
+	std::string art = parseart(leitor);
+	std::cout << art << std::endl; //Imprime o menu
 
 	leitor.close();
 }
@@ -46,14 +47,14 @@ void imprimeMenu()
 void codigoFuncao() {
 	char code = -2;
 	while (code == -2) {
-		cout << "Insira o Codigo de FunÃ§Ã£o: (-1 para Encerrar ExecuÃ§Ã£o)" << endl;
+		cout << "Insira o Codigo de Função: (-1 para Encerrar Execução)" << endl;
 		cin >> code;
 	}
 }
 
-
 int main()
 {
+
 	//imprimeMenu(); // Funcao para imprimir o Menu no Console
 
 	// Instanciando 7 tweets para teste
@@ -71,19 +72,19 @@ int main()
 	vetor[5] = new tweets(64, 189, t, d);
 	vetor[6] = new tweets(54, 28, t, d);
 
-	// ComeÃ§o das funÃ§oes apenas para testes de ordenacao
+	// Começo das funçoes apenas para testes de ordenacao
 	cout << "Antes de ordenar:" << endl;
 
 	for (int k = 0; k < 7; k++)
 	{
-		cout << "["<<vetor[k]->getTweetID()<<"]";
+		cout << "[" << vetor[k]->getTweetID() << "]";
 	}
-	
-	/* Deixar somente uma ordenaÃ§ao sem estar comentada, se nÃ£o todas as outras irÃ£o pegar o vetor jÃ¡ ordenado pela primeira para ordenar */
+
+	/* Deixar somente uma ordenaçao sem estar comentada, se não todas as outras irão pegar o vetor já ordenado pela primeira para ordenar */
 
 	/*QuickSort*/
 	QuickSort ordena; //Chama a classe
-	ordena.quicksort(vetor, 0, 6); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final
+	ordena.quicksort(vetor, 0, 6, 'r'); //Faz o quicksort. Passar sempre 0 como inicio e tamanho-1 como final
 
 	/*InsertionSort*/
 	//InsertionSort ordena; //Chama a classe
@@ -93,7 +94,7 @@ int main()
 	//BubbleSort ordena; //Chama a classe
 	//ordena.bubblesort(vetor, tam); //Faz o bubblesort. Passar o tamanho total, nao tamanho-1
 
-	cout << endl<< "Depois de ordenar:" << endl;
+	cout << endl << "Depois de ordenar:" << endl;
 	for (int p = 0; p < 7; p++)
 	{
 		cout << "[" << vetor[p]->getTweetID() << "]";
@@ -102,10 +103,10 @@ int main()
 	cout << endl << endl;
 	cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
 	cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
-	cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaÃ§ao estÃ¡ muito rapida com poucos elementos, tenho que testar com mais.
-	// Fim das funÃ§oes apenas para testes de ordenacao
+	cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos, tenho que testar com mais.
+	// Fim das funçoes apenas para testes de ordenacao
 
-	//codigoFuncao(); //Seleciona a funcao ou encerra a execuÃ§Ã£o;
+	//codigoFuncao(); //Seleciona a funcao ou encerra a execução;
 	system("pause"); //coloquei isso pq se nao fica sumindo a janela no visual studio, pode tirar
 	return 0;
 }
