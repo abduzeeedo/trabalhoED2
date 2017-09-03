ad6ddc1c776628ef94e3397f597a5316d625f66f
Bruno Carvalho <brunobcarvalho@live.com>
2017-09-03 16:31:28 -0300

    Menu Praticamente Finalizado

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "QuickSort.h"
 #include "InsertionSort.h"
 #include "MergeSort.h"
+#include "BubbleSort.h"
 
 using namespace std;
 
@@ -55,8 +56,8 @@ void imprimeVetor(tweets* vet[], int tam) {
 
 //Chama o quickSort passando o tipo por Parametro
 void tiposQuickSort(tweets* vet[], int tam, char tipo) {
+
 	/*QuickSort*/
-	cout << "Antes de Ordenar via QuickSort:" << endl;
 	imprimeVetor(vet, tam);
 	cout << endl;
 
@@ -82,18 +83,101 @@ void codigoFuncao(tweets* vet[], int tam) {
 		if (code == "-1") {
 			return;
 		}
+
 		if (code == "0") {
 			imprimeMenu();
 		}
+
 		if (code == "1") {
+			cout << "Antes de Ordenar via QuickSort Recursivo:" << endl;
 			tiposQuickSort(vet, tam, 'r'); //Tipo 'r': QuickSort Recursivo Padrao
 		}
 
 		if (code == "2") {
+			cout << "Falta Implementar" << endl;
+			/*
+			cout << "Antes de Ordenar via QuickSort Recursivo com Mediana entre 3 Valores:" << endl;
 			tiposQuickSort(vet, tam, 'm'); //Tipo 'm': QuickSort Recursivo com Mediana entre 3 valores
+			*/
 		}
+
 		if (code == "3") {
+			cout << "Falta Implementar" << endl;
+			/*
+			cout << "Antes de Ordenar via QuickSort Recursivo com Mediana entre 5 Valores:" << endl;
 			tiposQuickSort(vet, tam, 'M');//Tipo 'M': QuickSort Recursivo com Mediana entre 5 valores
+			*/
+		}
+
+		if (code == "4") {
+			cout << "Antes de Ordenar via QuickSort Recursivo com Inserção com m=10:" << endl;
+			tiposQuickSort(vet, tam, 'i');//Tipo 'M': QuickSort Recursivo com Inserção com m=10
+		}
+
+		if (code == "5") {
+			cout << "Antes de Ordenar via QuickSort Recursivo com Inserção com m=100:" << endl;
+			tiposQuickSort(vet, tam, 'I');//Tipo 'M': QuickSort Recursivo com Inserção com m=100
+		}
+
+		if (code == "6") {
+			/*InsertionSort*/
+			cout << "Antes de Ordenar via InsertionSort:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			InsertionSort ordena; //Chama a classe
+			ordena.insertionsort(vet, 0, tam); //Faz o insertionsort. Passar 0 para ordenar desde o inicio e o tamanho total, nao tamanho-1
+
+			cout << "Depois de Ordenar:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			//Resultado das Operações
+			cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
+			cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
+			cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos, tenho que testar com mais
+		}
+
+		if (code == "7") {
+			/*MergeSort*/
+			cout << "Antes de Ordenar via MergeSort:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			
+			MergeSort ordena; //Chama a classe 
+			ordena.mergesort(vet, 0, tam - 1); //Faz o mergesort. Passar sempre 0 como inicio e tamanho-1 como final
+
+			cout << "Depois de Ordenar:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			//Resultado das Operações
+			cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
+			cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
+			cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos
+		}
+		if (code == "8") {
+			/*HeapSort*/
+			cout << "Falta Implementar" << endl;
+		}
+		if (code == "9") {
+			/*BubbleSort*/
+			cout << "Antes de Ordenar via BubbleSort:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			BubbleSort ordena; //Chama a classe
+			ordena.bubblesort(vet, tam); //Faz o bubblesort. Passar o tamanho total, nao tamanho-1
+
+			cout << "Depois de Ordenar:" << endl;
+			imprimeVetor(vet, tam);
+			cout << endl;
+
+			//Resultado das Operações
+			cout << "Numero de trocas: " << ordena.getNumTrocas() << endl;
+			cout << "Numero de comparacoes: " << ordena.getNumComparacoes() << endl;
+			cout << "Tempo gasto: " << ordena.getTempoGasto() << endl; //Acho que o tempo sempre mostra 0 pois a ordenaçao está muito rapida com poucos elementos
 		}
 	}
 }
@@ -117,19 +201,6 @@ int main()
 	vetor[4] = new tweets(26, 16, t, d);
 	vetor[5] = new tweets(64, 189, t, d);
 	vetor[6] = new tweets(54, 28, t, d);
-
-	/*InsertionSort*/
-	//InsertionSort ordena; //Chama a classe
-	//ordena.insertionsort(vetor, 0, tam); //Faz o insertionsort. Passar 0 para ordenar desde o inicio e o tamanho total, nao tamanho-1
-
-	/*MergeSort*/
-	//MergeSort ordena; //Chama a classe 
-	//ordena.mergesort(vetor, 0, tam - 1); //Faz o mergesort. Passar sempre 0 como inicio e tamanho-1 como final
-
-	/*BubbleSort*/
-	//BubbleSort ordena; //Chama a classe
-	//ordena.bubblesort(vetor, tam); //Faz o bubblesort. Passar o tamanho total, nao tamanho-1
-
 
 	codigoFuncao(vetor, tam); //Seleciona a funcao ou encerra a execução;
 	//system("pause"); //coloquei isso pq se nao fica sumindo a janela no visual studio, pode tirar
