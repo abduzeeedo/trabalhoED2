a7a44f099a4f8e8f395743157f228aa224654abe
Bruno Carvalho <brunobcarvalho@live.com>
2017-09-03 14:39:15 -0300

    Ajuste do InsertionSort Pra qqr SubVetor

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -31,7 +31,7 @@ double InsertionSort::getTempoGasto()
 	return tempoGasto;
 }
 
-void InsertionSort::insertionsort(tweets* vet[], int tam)
+void InsertionSort::insertionsort(tweets* vet[], int inicio, int fim)
 {
 	clock_t relogio;
 	relogio = clock();
@@ -39,13 +39,13 @@ void InsertionSort::insertionsort(tweets* vet[], int tam)
 	int i, j;
 	tweets* chave; //chave auxiliar
 
-	for (i = 1; i < tam; i++)
+	for (i = inicio+1; i < fim; i++) //comeca da posicao inicio e termina na posicao fim
 	{
 		chave = vet[i]; //salva o valor atual do vet[i] na chave auxiliar
 		j = i - 1;
 
-		/* Move os elementos anteriores a chave que sao maiores que ela 1 posicao a frente da sua posicao atual*/
-		while (j >= 0 && (vet[j]->getTweetID() > chave->getTweetID() && ++numCompar))
+		/* Desloca os elementos anteriores a chave que sao maiores que ela 1 posicao a frente da sua posicao atual*/
+		while (j >= 0 && (vet[j]->getTweetID() > chave->getTweetID()) && ++numCompar)
 		{
 			vet[j + 1] = vet[j];
 			numTrocas++;
