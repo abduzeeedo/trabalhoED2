28cb9ce1dfbb0bda7336d76c857056ac8466e48c
rafaelstjf <rafaelstjf@hotmail.com>
2017-09-05 16:22:40 -0300

    sondagem linear

--- Hashing.cpp
+++ Hashing.cpp
@@ -12,65 +12,121 @@ Hashing::Hashing(int tamanho)
     }
 }
 //Funcoes de hashing
-int Hashing::divisao(int chave){
-    return chave%m;
+int Hashing::divisao(int chave)
+{
+    return chave % m;
 }
-int Hashing::enlacamento(int chave){
+int Hashing::enlacamento(int chave)
+{
     string str = to_string(chave);
     int enlacamento = 0;
-    for(int i = 0; i<str.length(); i++){
-        enlacamento+=atoi(str.substr(i, i+1).c_str());
+    for (int i = 0; i < str.length(); i++)
+    {
+        enlacamento += atoi(str.substr(i, i + 1).c_str());
     }
-    return enlacamento%m;
+    return enlacamento % m;
 }
-int Hashing::sondLinear(int chave){
-}
-int Hashing::sondQuadratica(int chave){
-
-}
-int Hashing::duploHash(int chave){
-
-}
-int Hashing::encadSeparado(int chave){
-
-}
-int Hashing::encadCol(int chave){
-
-}
-void Hashing::addVetor(int chave, int hashingFunc, int tratColis)
+void Hashing::sondLinear(int chave, int hashingFunc)
 {
-        int posicao = 0.0;
-        if(hashingFunc == 1)
-            posicao = divisao(chave);
-        else if(hashingFunc == 2)
-            posicao = enlacamento(chave);
-        if(table[posicao]==INF)
+    int posicao = 0;
+    int i = 0, it = 0;
+    int hk;
+    bool posicaovazia = false;
+    if (hashingFunc == 1)
+        hk = divisao(chave);
+    else if (hashingFunc == 2)
+        hk = enlacamento(chave);
+    while (!posicaovazia || it < 2)
+    {
+        posicao = (hk + i) % m;
+        if (table[posicao] == INF)
+        {
+            posicaovazia == true;
             table[posicao] = chave;
-        else{
-            switch(tratColis){
-                case 1:
+        }
+        else
+        {
+            i++;
+            if (i >= m)
+            {
+                i = 0;
+                it++;
+            }
+        }
+    }
+    void Hashing::sondQuadratica(int chave, int hashingFunc)
+    {
+        int posicao = 0;
+        int i = 0, it = 0;
+        int hk;
+        bool posicaovazia = false;
+        if (hashingFunc == 1)
+            hk = divisao(chave);
+        else if (hashingFunc == 2)
+            hk = enlacamento(chave);
+        while (!posicaovazia || it < 2)
+        {
+            posicao = (hk + i*i) % m;
+            if (table[posicao] == INF)
+            {
+                posicaovazia == true;
+                table[posicao] = chave;
+            }
+            else
+            {
+                i++;
+                if (i >= m)
+                {
+                    i = 0;
+                    it++;
+                }
+            }
+    }
+    void Hashing::duploHash(int chave, int hashingFunc)
+    {
+    }
+    void Hashing::encadSeparado(int chave, int hashingFunc)
+    {
+    }
+    void Hashing::encadCol(int chave, int hashingFunc)
+    {
+    }
+    void Hashing::addVetor(int chave, int hashingFunc, int tratColis)
+    {
+        int posicao = 0.0;
+        if (hashingFunc == 1)
+            posicao = divisao(chave);
+        else if (hashingFunc == 2)
+            posicao = enlacamento(chave);
+        if (table[posicao] == INF)
+            table[posicao] = chave;
+        else
+        {
+            switch (tratColis)
+            {
+            case 1:
                 posicao = sondLinear(chave);
                 break;
-                case 2:
+            case 2:
                 posicao = sondQuadratica(chave);
                 break;
-                case 3:
+            case 3:
                 posicao = duploHash(chave);
                 break;
-                case 4:
+            case 4:
                 posicao = encadSeparado();
                 break;
-                case 5:
+            case 5:
                 posicao = encadCol(chave);
                 break;
-                default:
+            default:
                 break;
                 table[posicao] = chave;
             }
         }
-}
+    }
 
-Hashing::~Hashing()
-{
-    delete[] table;
-}
+    Hashing::~Hashing()
+    {
+        delete[] table;
+    }
--- Hashing.h
+++ Hashing.h
@@ -14,7 +14,12 @@ private:
     int m;
     int* table;
     int divisao(int chave);
-    int enlacamento(int chave);
+    int enlacamento(int chave, int hashingFunc);
+    void sondLinear(int chave, int hashingFunc);
+    void sondQuadratica(int chave, int hashingFunc);
+    void duploHash(int chave, int hashingFunc);
+    void encadSeparado(int chave, int hashingFunc);
+    void encadCol(int chave, int hashingFunc);  
 public:
     Hashing(int tamanho);
     void addVetor(int chave, int hashingFunc, int tratColis);
