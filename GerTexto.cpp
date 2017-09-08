4f5590601d9f4a2d8f4e942be44845d6eaeb2d94
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-09-08 15:01:12 -0300

    gertexto criando um std::vector de tweets

--- GerTexto.cpp
+++ GerTexto.cpp
@@ -7,6 +7,7 @@ GerTexto::GerTexto()
 GerTexto::~GerTexto()
 {
 }
+
 Tweet *GerTexto::carregarTweet(string nome)
 {
     string linha;
@@ -24,4 +25,34 @@ Tweet *GerTexto::carregarTweet(string nome)
 		tw->setDate(linha);
     }
 return tw;
+}
+
+//Carrega um vetor de tweets com numTweets tweets
+vector<Tweet*> GerTexto::carregaTweets(string nomeArquivo, int numTweets)
+{
+	fstream arquivo(nomeArquivo);
+	vector<Tweet*> vTweets(numTweets);
+	string linha;
+	int t = 0;
+	if (arquivo.is_open()) //Verifica se o arquivo esta aberto
+	{
+		while (!arquivo.eof() && t < numTweets) //Continua lendo o arquivo
+		{
+			Tweet *tw = new Tweet();
+			getline(arquivo, linha, '\t');
+			tw->setUserID(stoi(linha));
+			getline(arquivo, linha, '\t');
+			tw->setTweetID(atoi(linha.c_str()));
+			getline(arquivo, linha, '\t');
+			tw->setTweetText(linha);
+			getline(arquivo, linha);
+			tw->setDate(linha);
+			vTweets[t] = tw;
+			t++;
+		}
+		arquivo.close(); //Fecha o arquivo
+		return vTweets; //Retorna o vetor
+	}
+	else //Caso o arquivo nao esteja aberto, mostra um erro.
+		cout << "Erro ao abrir arquivo de tweets "<< nomeArquivo << endl;
 }
