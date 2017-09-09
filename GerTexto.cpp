5cf8ee36ef99f18cbdc528005b2572a895c747c1
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-09-09 11:58:44 -0300

    gertexto carregando varios tweets

--- GerTexto.cpp
+++ GerTexto.cpp
@@ -42,6 +42,38 @@ Tweet *GerTexto::carregarTweet(string nome, int seed)
 	}
 	return tw;//retorna um tweet com tais valores recebidos do arquivo txt
 }
+
+//Carrega um vetor de tweets com numTweets tweets, os tweets sao carregados em sequencia
+Tweet** GerTexto::carregaTweets(string nomeArquivo, int numTweets)
+ {
+	fstream arquivo(nomeArquivo);
+	Tweet** ordenaEntrada = new Tweet*[numTweets];
+	string linha;
+	int t = 0;
+	if (arquivo.is_open()) //Verifica se o arquivo esta aberto
+		 {
+		while (!arquivo.eof() && t < numTweets) //Continua lendo o arquivo
+			 {
+				int uID;
+				long long int tID;
+				string text, date;
+				arquivo >> uID >> tID;
+				std::getline(arquivo, linha, '\t');//Pega o espaco em branco, se tiver
+				std::getline(arquivo, text, '\t');
+				std::getline(arquivo, date);
+				Tweet* tw = new Tweet(uID, tID, text, date);
+				ordenaEntrada[t] = tw;
+				t++;
+				cout << "tweet numero " << t << " carregado";
+				cout << endl;
+			}
+		arquivo.close(); //Fecha o arquivo
+		return ordenaEntrada; //Retorna o vetor
+		}
+	else //Caso o arquivo nao esteja aberto, mostra um erro.
+		 cout << "Erro ao abrir arquivo de tweets " << nomeArquivo << endl;
+}
+
 void GerTexto::salvarSaida(string saida, string nome){
 	fstream arquivo;
 	arquivo.open(nome,std::fstream::out | std::fstream::app);
