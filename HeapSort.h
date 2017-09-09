7d4916525e1a91c2d7edb64dda1bb2555fdf4e67
diogodestefano <31486694+diogodestefano@users.noreply.github.com>
2017-09-09 17:49:51 -0300

    Update HeapSort.h

--- HeapSort.h
+++ HeapSort.h
@@ -14,8 +14,8 @@ public:
 	int getNumTrocas();
 	int getNumComparacoes();
 	double getTempoGasto();
-	HeapSort::HeapSort(Tweet vetor[], int tam);
+	void limpaDados();
+	void heapsort(Tweet vetor[], int tam);
 	void constroiHeap(Tweet vetor[], int tam, int indice_raiz);
 
 };
-
