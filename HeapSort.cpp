51ee24335e7e09c4f073e3c679c2b9c1510c7717
diogodestefano <31486694+diogodestefano@users.noreply.github.com>
2017-09-09 17:31:21 -0300

    Add files via upload

---
+++ HeapSort.cpp
@@ -0,0 +1,84 @@
+#include "HeapSort.h"
+
+using namespace std;
+
+//Construtor
+HeapSort::HeapSort()
+{
+	numTrocas = 0;
+	numCompar = 0;
+	tempoGasto = 0.0;
+}
+
+//Destrutor
+HeapSort::~HeapSort()
+{
+}
+
+//Retorna o numero de trocas feitas
+int HeapSort::getNumTrocas()
+{
+	return numTrocas;
+}
+
+//Retorna o numero de comparacoes feitas
+int HeapSort::getNumComparacoes()
+{
+	return numCompar;
+}
+
+//Retorna o tempo gasto
+double HeapSort::getTempoGasto()
+{
+	return tempoGasto;
+}
+
+ //Constroi um vetor Heap
+void constroiHeap(Tweet vetor[], int tam, int indice_raiz)
+{
+	int ramificacao;
+	Tweet valor;
+	valor = vetor[indice_raiz];
+
+	while (indice_raiz < tam / 2) {
+		ramificacao = 2 * indice_raiz + 1;
+
+		if (ramificacao < tam - 1 && vetor[ramificacao].getTweetID()  < vetor[ramificacao + 1].getTweetID())
+			ramificacao++;
+
+		if (valor.getTweetID() >= vetor[ramificacao].getTweetID())//Identifica o max-heap
+			break;
+
+		vetor[indice_raiz] = vetor[ramificacao];
+		indice_raiz = ramificacao;
+		++numCompar;
+	}
+	vetor[indice_raiz] = valor;
+}
+
+
+
+
+//Realiza as trocas
+void HeapSort(Tweet vetor[], int tam)
+{
+	clock_t relogio;
+	relogio = clock();
+
+	int indice;
+	Tweet troca;
+	for (indice = tam / 2; indice >= 0; indice--)
+		constroiHeap(vetor, tam, indice);
+
+	while (tam > 0)
+	{
+		troca = vetor[0];
+		vetor[0] = vetor[tam - 1];
+		vetor[tam - 1] = troca;
+		numTrocas++;
+		constroiHeap(vetor, --tam, 0);
+	}
+
+	tempoGasto = (clock() - relogio) / (double)CLOCKS_PER_SEC;
+
+}
