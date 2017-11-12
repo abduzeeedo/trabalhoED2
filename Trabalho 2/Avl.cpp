0e4d0c8a1eedd85bd695a21cd7ae963153335c2c
Rafael Terra <rafaelstjf@users.noreply.github.com>
2017-11-12 19:44:03 -0200

    consertos avl

--- Trabalho 2/Avl.cpp
+++ Trabalho 2/Avl.cpp
@@ -22,7 +22,7 @@ AVL::~AVL()
 {
 	destrutorAux(raiz);
 }
-void AVL::destrutorAux(No* no)
+void AVL::destrutorAux(No *no)
 {
 	if (no != NULL)
 	{
@@ -32,34 +32,39 @@ void AVL::destrutorAux(No* no)
 	}
 }
 //rotacoes
-No* AVL::rotR(No* no) //rotacao direita
+No *AVL::rotR(No *no) //rotacao direita
 {
-	No* noAux = no->getEsq();
+	No *noAux = no->getEsq();
+	numCopias++;
 	no->setEsq(noAux->getDir());
 	noAux->setDir(no);
 	return noAux;
 }
 
-No* AVL::rotRR(No* no) //rotacao dupla direita
+No *AVL::rotRR(No *no) //rotacao dupla direita
 {
 	no->setEsq(rotL(no->getEsq()));
-	No* noAux = rotR(no);
+	No *noAux = otR(no);
+	numCopias++;
 	return noAux;
 }
 
-No* AVL::rotL(No* no) //rotacao esquerda
+No *AVL::rotL(No *no) //rotacao esquerda
 {
-	No* noAux = no->getDir();
-	No* noAux2 = noAux->getEsq();
+	No *noAux = no->getDir();
+	No *noAux2 = noAux->getEsq();
+	numCopias++;
+	numCopias++;
 	no->setDir(noAux2);
 	noAux->setEsq(no);
 	return noAux;
 }
 
-No* AVL::rotLL(No* no) //rotacao dupla esquerda
+No *AVL::rotLL(No *no) //rotacao dupla esquerda
 {
 	no->setDir(rotR(no->getDir()));
-	No* noAux = rotL(no);
+	No *noAux = rotL(no);
+	numCopias++;
 	return noAux;
 }
 //Buscas
@@ -67,32 +72,37 @@ bool AVL::busca(int tweetId)
 {
 	return buscaAuxiliar(tweetId, raiz);
 }
-bool AVL::buscaAuxiliar(int tweetId, No* no)
+bool AVL::buscaAuxiliar(int tweetId, No *no)
 {
 	clock_t relogio;
 	relogio = clock();
 
-	if (no == NULL) {
+	if (no == NULL)
+	{
 		numCompar++;
 		tempoGastoBusca += (clock() - relogio) / (double)CLOCKS_PER_SEC;
 		return false;
 	}
-	else if (no->getChave()->getTweetID() == tweetId){
+	else if (no->getChave()->getTweetID() == tweetId)
+	{
 		numCompar++;
 		tempoGastoBusca += (clock() - relogio) / (double)CLOCKS_PER_SEC;
 		return true;
 	}
 	else if (no->getChave()->getTweetID() != tweetId)
 	{
-		if (no->getEsq() != NULL && no->getChave()->getTweetID() < tweetId){
+		if (no->getEsq() != NULL && no->getChave()->getTweetID() < tweetId)
+		{
 			numCompar++;
 			return buscaAuxiliar(tweetId, no->getEsq());
 		}
-		else if (no->getDir() != NULL && no->getChave()->getTweetID() > tweetId){
+		else if (no->getDir() != NULL && no->getChave()->getTweetID() > tweetId)
+		{
 			numCompar++;
 			return buscaAuxiliar(tweetId, no->getDir());
 		}
-		else if (no->getEsq() == NULL && no->getDir() == NULL){
+		else if (no->getEsq() == NULL && no->getDir() == NULL)
+		{
 			numCompar++;
 			tempoGastoBusca += (clock() - relogio) / (double)CLOCKS_PER_SEC;
 			return false;
@@ -101,7 +111,7 @@ bool AVL::buscaAuxiliar(int tweetId, No* no)
 }
 
 //Manipulacao da arvore
-void AVL::inserir(Tweet* tw)
+void AVL::inserir(Tweet *tw)
 {
 	clock_t relogio;
 	relogio = clock();
@@ -110,7 +120,7 @@ void AVL::inserir(Tweet* tw)
 	calculaFB(raiz);
 	tempoGastoInsercao += (clock() - relogio) / (double)CLOCKS_PER_SEC;
 }
-void AVL::remover(Tweet* tw)
+void AVL::remover(Tweet *tw)
 {
 	clock_t relogio;
 	relogio = clock();
@@ -119,10 +129,10 @@ void AVL::remover(Tweet* tw)
 	calculaFB(raiz);
 	tempoGastoRemocao += (clock() - relogio) / (double)CLOCKS_PER_SEC;
 }
-No* AVL::removerAux2(No* no)
+No *AVL::removerAux2(No *no)
 {
-	No* noAux;
-	No* noAuxPai = new No();
+	No *noAux;
+	No *noAuxPai = new No();
 	if ((no->getEsq() == NULL) && (no->getDir() == NULL))
 	{
 		delete no;
@@ -131,28 +141,44 @@ No* AVL::removerAux2(No* no)
 	else if ((no->getEsq() == NULL) && (no->getDir() != NULL))
 	{
 		noAux = no->getEsq();
+		numCopias++;
 		delete no;
 		return noAux;
 	}
 	else if ((no->getEsq() != NULL) && (no->getDir() == NULL))
 	{
 		noAux = no->getDir();
+		numCopias++;
 		delete no;
 		return noAux;
 	}
 	else
 	{
-		noAux = no->getEsq();
-		while (noAux->getDir() != NULL)
+		if (no->getEsq()->getDir() == NULL)
 		{
-			noAuxPai = noAux;
-			noAux = noAux->getDir();
+			noAux = no->getEsq();
+			numCopias++;
+			no->getEsq()->setDir(no->getDir());
+			delete no;
+			delete noAux;
 		}
-		if (noAux->getEsq() != NULL)
-			noAuxPai->setDir(noAux->getEsq());
 		else
 		{
-			noAuxPai->setDir(NULL);
+			noAux = no->getEsq();
+			numCopias++;
+			while (noAux->getDir() != NULL)
+			{
+				noAuxPai = noAux;
+				numCopias++;
+				noAux = noAux->getDir();
+				numCopias++;
+			}
+			if (noAux->getEsq() != NULL)
+				noAuxPai->setDir(noAux->getEsq());
+			else
+			{
+				noAuxPai->setDir(NULL);
+			}
 			noAux->setDir(no->getDir());
 			noAux->setEsq(no->getEsq());
 			delete no;
@@ -161,12 +187,14 @@ No* AVL::removerAux2(No* no)
 	}
 	return NULL;
 }
-No* AVL::removerAux(No* no, Tweet* tw)
+No *AVL::removerAux(No *no, Tweet *tw)
 {
-	No* noAux = no;
-	No* noPai = new No();
+	No *noAux = no;
+	numCopias++;
+	No *noPai = new No();
 	bool bdir = false;
-	if (tw->getTweetID() == no->getChave()->getTweetID()) return removerAux2(noAux);
+	if (tw->getTweetID() == no->getChave()->getTweetID())
+		return removerAux2(noAux);
 	while (noAux != NULL)
 	{
 		if (tw->getTweetID() == noAux->getChave()->getTweetID())
@@ -183,25 +211,30 @@ No* AVL::removerAux(No* no, Tweet* tw)
 			{
 				bdir = false;
 				noPai = noAux;
+				numCopias++;
 				noAux = noAux->getEsq();
+				numCopias++;
 			}
 			else
 			{
 				bdir = true;
 				noPai = noAux;
+				numCopias++;
 				noAux = noAux->getDir();
+				numCopias++;
 			}
 		}
 	}
 	return no;
 }
-void AVL::inserirAuxiliar(Tweet* tw, No* no)
+void AVL::inserirAuxiliar(Tweet *tw, No *no)
 {
 	if (no == NULL)
 	{
 		numCompar++;
 		no = new No();
 		no->setChave(tw);
+		numCopias++;
 	}
 	else
 	{
@@ -215,9 +248,7 @@ void AVL::inserirAuxiliar(Tweet* tw, No* no)
 		{
 			numCompar++;
 			inserirAuxiliar(tw, no->getDir());
-
 		}
-
 	}
 }
 void AVL::limpaDados()
@@ -229,7 +260,7 @@ void AVL::limpaDados()
 }
 
 //Outras funcoes
-void AVL::calculaFB(No* no)
+void AVL::calculaFB(No *no)
 {
 	if (no != NULL)
 	{
@@ -237,20 +268,19 @@ void AVL::calculaFB(No* no)
 		numCompar++;
 	}
 }
-int AVL::calculaAltura(No* no)
+int AVL::calculaAltura(No *no)
 {
 	int altEsq, altDir;
 	if (no == NULL)
 	{
 		numCompar++;
 		return 0;
-
 	}
 	else
 	{
 		altEsq = calculaAltura(no->getEsq());
 		altDir = calculaAltura(no->getDir());
-		if (altEsq>altDir)
+		if (altEsq > altDir)
 		{
 			numCompar++;
 			return (1 + altEsq);
@@ -260,10 +290,9 @@ int AVL::calculaAltura(No* no)
 			numCompar++;
 			return (1 + altDir);
 		}
-
 	}
 }
-No* AVL::balanceia(No* no)
+No *AVL::balanceia(No *no)
 {
 	if (no != NULL)
 	{
@@ -272,16 +301,18 @@ No* AVL::balanceia(No* no)
 		{
 			numCompar++;
 			calculaFB(no->getEsq());
-			if (no->getEsq()->getFB()>0)
+			if (no->getEsq()->getFB() > 0)
 			{
 				numCompar++;
 				no = rotR(no);
+				numCopias++;
 			}
 
 			else
 			{
 				numCompar++;
 				no = rotRR(no);
+				numCopias++;
 			}
 		}
 		else if (no->getFB() == -2)
@@ -292,12 +323,13 @@ No* AVL::balanceia(No* no)
 				numCompar++;
 
 				no = rotL(no);
+				numCopias++;
 			}
 			else
 			{
 				no = rotLL(no);
 				numCompar++;
-
+				numCopias++;
 			}
 		}
 		no->setEsq(balanceia(no->getEsq()));
@@ -309,7 +341,7 @@ void AVL::imprimir()
 {
 	imprimirAux(raiz);
 }
-void AVL::imprimirAux(No* no)
+void AVL::imprimirAux(No *no)
 {
 	if (no != NULL)
 	{
@@ -338,7 +370,6 @@ long int AVL::getNumCopias()
 double AVL::getTempoInsercao()
 {
 	return tempoGastoInsercao;
-
 }
 double AVL::getTempoRemocao()
 {
