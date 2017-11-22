1ebddd0109b139b1e67d5464c6ebe5d786986725
rafaelstjf <rafaelstjf@hotmail.com>
2017-11-22 15:19:27 -0200

    Pasta Trabalho 3

---
+++ Trabalho 3/LZ77.h
@@ -0,0 +1,13 @@
+#ifndef LZ77_H
+#define LZ77_H
+
+class LZ77
+{
+  private:
+
+  public:
+  LZ77();
+  ~LZ77();
+};
+
+#endif
---
+++ Trabalho 3/main.cpp
@@ -0,0 +1,22 @@
+﻿/*---------------------------------------
+TRABALHO DE ESTRUTURA DE DADOS 2
+GRUPO:
+Bruno Carvalho
+Diogo Destefano
+Rafael Terra
+Pedro Bellotti
+--------------------------------------*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+#include "Tweet.h"
+#include "GerTexto.h"
+
+using namespace std;
+
+int main(){
+	return 0;
+}
