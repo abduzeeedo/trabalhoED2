229946260cb41183a7a11350893f7a7d96a65b54
rafaelstjf <rafaelstjf@hotmail.com>
2017-12-03 10:56:38 -0200

    Revert "lz77"
    This reverts commit e85f07a3772e9a7b9b04d5e76752fb12d3c09202.

--- Trabalho 3/LZ77.cpp
+++ Trabalho 3/LZ77.cpp
@@ -1,102 +1,45 @@
 #include "LZ77.h"
 
-LZ77::LZ77(int tamDic, int tamBuffer)
+LZ77::LZ77()
 {
-    this->tamDic = tamDic;
-    this->tamBuffer = tamBuffer;
+    tamDic = 18;
+    tamBuffer = 8;
+    buffer = new char[tamBuffer];
+    dicionario = new char[tamDic];
 }
 LZ77::~LZ77()
 {
-
+    delete[] buffer;
+    delete[] dicionario;
 }
-vector<Tupla*> LZ77::comprimirPalavra(string palavra)
+string LZ77::comprimirPalavra(string palavra)
 {
-    char buffer[tamBuffer];
-    char dicionario[tamDic];
-    for(int k = 0; k<tamBuffer; k++)
-        buffer[k] = '\0';
-    for(int k = 0; k<tamDic; k++)
-        dicionario[k] = '\0';
-    char literal;
-    int i = 0;
-    int begin_window;
-    int index = 0;
-    string output;
-    vector<Tupla*> tuplas;
-    while(i < palavra.size())
+    vector<Tripla> triplas;
+    limpaDicionario();
+    LimpaBuffer();
+    for(int i = 0; i<palavra.size(); i++)
     {
-        begin_window = i-tamDic;
-        if(begin_window<0)
+        if(i==0)
         {
-            begin_window = 0;
-        }
-        for(int j = 0; j<tamDic; j++)
-        {
-            if(i>=tamDic)
-            {
-                begin_window = i-tamDic;
-                dicionario[j] = palavra[j+begin_window];
-            }
-            else
-            {
-                if(j>=i)
-                    break;
-                else
-                    dicionario[j] = palavra[j];
-            }
+            for(int j = 0; j<tamBuffer; j++) //coloca os caracteres no buffer
+                buffer[j] = palavra[j];
 
         }
-        for(int j = 0; j<(tamBuffer); j++)
-            buffer[j] = palavra[i+j];
-        Tupla* t = new Tupla();
-        t->setP(0);
-        t->setL(0);
-        t->setC(palavra[i]);
-        string strDic(dicionario);
-        string strBuffer(buffer);
-        cout << "Dic: " << strDic << " |Buffer: " << strBuffer << endl;
-        for(int j = tamBuffer-1; j>0; j--)
-        {
 
-            string strBuffer2(buffer, j);
-            index = strDic.rfind(strBuffer2);
-            cout << index << endl;
-            if(index >= 0)
-            {
-                literal = '\0';
-                if(i+j < palavra.size())
-                {
-                    literal = palavra[i+j];
-                }
-                t->setP(strDic.size() - index - 1);
-                t->setL(j);
-                t->setC(literal);
-                break;
-            }
-        }
-        i = i + t->getL() +1;
-        tuplas.push_back(t);
     }
-
-    for(int k = 0; k<tuplas.size(); k++)
-    {
-        cout <<"(" << tuplas[k]->getP() << ", "<< tuplas[k]->getL() << ", " << tuplas[k]->getC() << ")" << endl;;
-    }
-    return tuplas;
 }
 
-string LZ77::descomprimirPalavra(vector<Tupla*> tuplas)
+string LZ77::descomprimirPalavra(string palavra)
 {
-    string output;
-    int pos;
-    for(int i = 0; i<tuplas.size(); i++)
-    {
-        pos = output.size() - tuplas[i]->getP() -1;
-        for(int j = pos; j<pos+tuplas[i]->getL(); j++)
-            output+=(output[j]);
-        output+=(tuplas[i]->getC());
 
-    }
-     cout << output << endl;
-    return output;
+}
+void LZ77::limpaDicionario()
+{
+    for(int i = 0; i<tamDic; i++)
+        dicionario[i] = '\0';
+}
+void LZ77::LimpaBuffer()
+{
+    for(int i = 0; i<tamBuffer; i++)
+        buffer[i] = '\0';
 }
--- Trabalho 3/main.cpp
+++ Trabalho 3/main.cpp
@@ -15,18 +15,265 @@ Pedro Bellotti
 #include <algorithm>
 #include "Tweet.h"
 #include "GerTexto.h"
-#include "LZ77.h"
-#include "Tupla.h"
+
 string saida = "";
 using namespace std;
 
 //Funcao usada para pegar as linhas do arquivo do Menu.
 //Entrada: Arquivo .txt com os dados do Menu
 //Saída: String lida na linha analizada.
+string analisaLinhas(ifstream& File)
+{
+	string linhaVerif;
 
-int main(){
-	LZ77* teste = new LZ77(6,4);
-	vector<Tupla*> t;
-	t = teste->comprimirPalavra("bananabanabofana");
-	cout << teste->descomprimirPalavra(t);
+	if (File)
+	{
+		while (File.good())
+		{
+			string linhaTemp;
+			getline(File, linhaTemp);
+			linhaTemp += "\n";
+
+			linhaVerif += linhaTemp;
+		}
+		return linhaVerif;
+	}
+	else
+	{
+		cout << "ERRO AO LER O ARQUIVO DE MENU" << endl;
+		return 0;
+	}
+}
+
+//Funcao Que salva uma string em um arquivo .txt
+//Entrada: String a ser salva e arquivo .txt onde os dados serao salvos.
+//Saida: Escrita da string passada por parametro em um arquivo .txt (tambem passado por parametro)
+void salvarTxt(string salvar, string file) {
+	ofstream arquivo;
+	arquivo.open(file);
+	arquivo << salvar << endl;
+	//arquivo.close();
+}
+
+//fun��o para converter um int para String, usada na escrita de dados em arquivo txt
+//Entrada: Numero Inteiro -- Saida: Representacao deste numero inteiro em uma String
+string toString(double i)
+{
+	stringstream ss;
+	ss << i;
+	string retorno = ss.str();
+	return retorno;
+}
+
+//Funcao de Impressao de Menu
+//Entrada: Arquivo .txt do menu a ser impresso em tela
+//Saida: Impressao em tela do Menu de opcoes
+void imprimeMenu()
+{
+	ifstream leitor("menu.txt"); //Le o menu to txt
+	string art = analisaLinhas(leitor);
+	cout << art << endl; //Imprime o menu
+
+	leitor.close();
+}
+
+//Funcao usada para imprimir todos os TweetID`s
+//Entrada: Vetor de objetos do tipo Tweet e tamanho de tal vetor.
+//Saida: Impressao em tela de todos os TweetID`s deste vetor
+void imprimeTIDVetor(Tweet* vet[], int tam) {
+	cout << "Vetor: ";
+	for (int k = 0; k < tam; k++)
+	{
+		cout << "[" << vet[k]->getTweetID() << "]";
+	}
+	cout << endl;
+}
+
+//Funcao passada um ponteiro pra vetor do tipo Tweet, retorna um ponteiro pra um vetor com os mesmos valores com tamanho tam
+//Entrada: Ponteiro para vetor do tipo Tweet, tamanho do vetor origem e destino
+//Saida: Retorna um ponteiro para um vetor com "tam" objetos
+Tweet** carregaTweets(Tweet** src, int tam, int tamVet) {
+	Tweet** dst = new Tweet*[tam];
+	for (int i = 0; i < tam; i++) {
+		dst[i] = src[i];
+	}
+	return dst;
+}
+
+//Funcao randomiza o conteudo de um vetor de tweets
+//ENTRADA: Ponteiro para vetor do tipo Tweet e tamanho do vetor
+//SAIDA: O vetor de tweets com valores entre as posicoes randomizados (desordenado)
+void randomiza(Tweet** vetor, int tam) {
+	for (int i = 0; i < tam; i++)
+	{
+		srand(2 * i + tam); //Troca a seed do rand a cada iteraçao
+		swap(vetor[rand() % tam], vetor[rand()*(i + 2) % tam]);
+	}
+}
+
+//Funcao para ler os N numeros aleatorios do arquivo entrada.txt
+//ENTRADA: Arquivo .txt com dados de N numero aleatorios a serem inseridos
+//SAIDA: Vetor do tipo Vector onde cada posicao contem um nemero contido em uma linha do arquivo passado por parametro
+vector<int> importaEntrada(const char* nomeArquivo)
+{
+	int i = 0; //Variavel para controlar a posicao do vetor
+	string linha; //Variavel para armazenar a linha no getline
+	ifstream arquivo(nomeArquivo); //Abre o arquivo
+	int N; //Tamanho do vetor
+	if (arquivo.is_open()) //Verifica se o arquivo esta aberto
+	{
+		getline(arquivo, linha); //Le a primeira linha do arquivo
+		N = atoi(linha.c_str()); //Converte string para int
+		vector<int> vEntrada(N); //Cria o vetor com N posicoes lidas da primeira linha
+		while (!arquivo.eof()) //Continua lendo o arquivo
+		{
+			getline(arquivo, linha);
+			vEntrada[i] = atoi(linha.c_str()); //Armazena na posicao i do vetor o numero lido da linha
+			i++;
+		}
+		arquivo.close(); //Fecha o arquivo
+		return vEntrada; //Retorna o vetor
+	}
+	else //Caso o arquivo nao esteja aberto, mostra um erro.
+		cout << "Erro ao abrir arquivo " << nomeArquivo << endl;
+}
+
+//Remove os espacos a mais de uma string, feito in-place e O(n) onde n eh o tamanho da string (tweet)
+//ENTRADA: uma string (tweet)
+//SAIDA: a mesma string, sem espacos desnecessarios (apenas um espaco entre cada palavra)
+void removeEspaco(string &str)
+{
+	//i = posicao vazia na "nova" string
+	//j = posicao da proxima letra a ser inserida na string original
+	//tam = tamanho da string (condicao de parada)
+	//espaco = flag para nao retirar espacos necessarios (precisa ter pelo menos um espaco entre cada palavra)
+	size_t tam = str.length();
+	short i = 0;
+	short j = -1;
+	bool espaco = false;
+
+	//Percorre por todos os espacos no comeco do texto, se houver algum
+	if (str[++j] == ' ')
+		while (++j < tam && str[j] == ' ');
+
+	//Comeca a leitura da string
+	while (j < tam)
+	{
+		if (str[j] != ' ')
+		{
+			str[i++] = str[j++]; //Coloca a letra da posicao j na posicao i e aumenta os indices
+			espaco = false; //Quando o caractere encontrado e uma letra, coloca a flag como false
+		}
+		else if (str[j++] == ' ')
+		{
+			if (!espaco)
+			{
+				str[i++] = ' '; //Adiciona apenas um espaco (necessario) entre uma palavra e outra
+				espaco = true; //Quando o caractere encontrado nao e uma letra, coloca a flag como true
+			}
+		}
+	}
+	//Remove todos os epacos no final do texto, se houver algum
+	if (i <= 1)
+		str.erase(str.begin() + i, str.end());
+	else
+	{
+		if (str[i - 1] == ' ')
+			str.erase(str.begin() + i - 1, str.end());
+		else
+			str.erase(str.begin() + i, str.end());
+	}
+}
+
+//Verifica se a letra e valida (se e de A a Z ou um numero)
+//ENTRADA: uma variavel do tipo const char
+//SAIDA: verdadeiro/falso dependendo se a variavel eh uma letra/numero ou nao
+bool charInvalido(const char &c)
+{
+	if ((c <= 'Z' && c >= 'A') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+		return false;
+	else
+		return true;
+}
+
+//Remove todas as pontuacoes, espacos, caracteres especiais e coloca todas as letras em minusculo
+//ENTRADA: Uma string (tweet)
+//SAIDA: Uma string contendo as mesmas palavras da entrada porem em letras minusculas e sem sinais de pontuacao.
+string limpaString(string original)
+{
+	string s = original;
+	/*Nao usei o erase/remove porque em tweets que nao existe espacos entre a pontuacao, as palavras ficariam todas juntas dificultando dividir o tweet em varias palavras
+	Exemplo: usando erase/remove no tweet Exemplo.de.tweet, iria ficar Exemplodetweet e isso ia contar como uma palavra so
+	Usando o replace_if com espacos, ficaria Exemplo de tweet, o que eh o certo*/
+	transform(s.begin(), s.end(), s.begin(), ::tolower);//Coloca as letras em minusculo
+	replace_if(s.begin(), s.end(), charInvalido, ' ');//Troca todos os sinais de pontuacao e caracteres especiais por espacos
+	removeEspaco(s);//Remove os espacos desneessarios
+	return s;
+}
+
+//Metodo que seleciona via codigo de comando a funcao a ser executada e finaliza execucao
+//ENTRADA: Vetor de objetos do tipo Tweet e o tamanho deste vetor
+//SAIDA: Chamada de funcoes de acordo com o codigo inserido pelo usuario
+void codigoFuncao(Tweet* vet[], int tam) {
+	string code;
+	while (true) {
+		cout << endl << "Insira o Codigo de Funcao: (-1 para Encerrar Execucao, 0 Para imprimir Menu)" << endl;
+		cin >> code;
+		if (code == "-1") {
+			return; //Finaliza a Execucao
+		}
+
+		if (code == "0") {
+			imprimeMenu();//Imprime o Menu (via arquivo txt)
+		}
+
+		if (code == "1") {
+			cout << "Fazendo a compressao de tweets usando metodo Huffman." << endl;
+		}
+
+		if (code == "2") {
+			cout << "Fazendo a compressao de tweets usando metodo LZ77." << endl;
+		}
+
+		if (code == "3") {
+			cout << "Fazendo a compressao de tweets usando metodo LZ78." << endl;
+		}
+
+		if (code == "4") {
+			cout << "Fazendo a compressao de tweets usando metodo LZW." << endl;
+		}
+	}
+}
+
+int main()
+{
+	int i; //Variavel para controle de iteracoes
+	imprimeMenu(); // Funcao para imprimir o Menu no Console
+
+	/*Essa funcao sera usada para importar os tweets, cada posicao do vetor contem um numero
+	que indica o numero de tweets aleatorios que devem ser importados e instanciados
+	para serem comprimidos*/
+	vector<int> vEntrada = importaEntrada("entrada.txt");
+
+	//Importando tweets do arquivo TXT-------------------------------------------
+	int tamVet = 100; //Quantidade de Tweets que serao lidos do arquivo txt
+	GerTexto* ger = new GerTexto();
+	cout << "[1] Instanciando " << tamVet << " tweets para realizar os testes, aguarde." << endl;
+	Tweet** vTweet = ger->carregaTweets("test_set_tweets.txt", tamVet);
+	//---------------------------------------------------------------------------
+
+	//Randomizando o vetor de entrada para fazer o calculo da frequencia de N tweets aleatorios
+	randomiza(vTweet, tamVet);
+
+	/* Para facilitar na compressao, removemos todos os caracteres que nao sao letras ou numeros das strings
+	e tambem colocamos todas as letras em minusculo */
+	cout << "[2] Retirando todos os caracteres especiais, sinais de pontuacao e colocando todos os caracteres em minusculo." << endl;
+	for (i = 0; i < tamVet; i++)
+		vTweet[i]->setTweetText(limpaString(vTweet[i]->getTweetText()));
+
+
+	codigoFuncao(vTweet, tamVet);//Seleciona a funcao ou encerra a execucao;
+	salvarTxt(saida, "saidas.txt");
+	system("pause");
+	return 0;
 }
