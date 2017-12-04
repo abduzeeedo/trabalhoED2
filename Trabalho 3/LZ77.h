321c67449de38befdf95bd79d56d324915887bf9
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2017-12-04 10:46:24 -0200

    alteracoes finais

--- Trabalho 3/LZ77.h
+++ Trabalho 3/LZ77.h
@@ -1,3 +1,11 @@
+/*---------------------------------------
+TRABALHO DE ESTRUTURA DE DADOS 2
+GRUPO:
+Bruno Carvalho
+Diogo Destefano
+Rafael Terra
+Pedro Bellotti
+--------------------------------------*/
 #ifndef LZ77_H
 #define LZ77_H
 
